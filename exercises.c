28ec53ed3120c76e9d8f2901128e4976ad60de2d
Your Name <you@example.com>
2024-03-14 14:56:08 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -91,10 +91,6 @@ y luego devuelva 1 si el arreglo está ordenado en orden ascendente,
   0 si no está ordenado, y -1 si está ordenado en orden descendente.
 */
 int checkSorted(int arr[], int size) { 
-  for (unsigned int i = 1 ; i < size ; i++)
-    {
-      if (arr[i] )
-    }
   return -2; }
 
 /*
