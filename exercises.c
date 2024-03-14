3cac7673e44c1ab58761423af857873ce7ff6889
Your Name <you@example.com>
2024-03-14 15:08:14 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -69,7 +69,6 @@ void mergeSortedArrays(int arr1[], int size1, int arr2[], int size2,
                        int result[]) {
   int j = 0;
   int k = 0;
-  result = (int *)malloc(size1 + size2 * sizeof(int));
   for (unsigned int i = 0 ; i < size1 + size2 ; i++)
     {
       if(arr1[j] < arr2[k]) {
